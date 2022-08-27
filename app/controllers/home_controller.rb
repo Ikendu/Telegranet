9b3a46ab501f3eebee7eab0de8fdaf7f1156d517
aniede <ndubest56@gmail.com>
2022-08-27 20:02:10 +0100

    post scafold

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,22 @@ class HomeController < ApplicationController
   end
 
   def group_message
-    HTTParty.post('https://api.telegram.org/bot5609590504:AAHWdpmAzwhmqcUPp0MbSHgOq6OfLvWX0lw/sendMessage?chat_id=5333385500&text=group message')
+    text = "Chibundu"
+
+  # HTTParty.post("https://api.telegram.org/bot#{@api_key}/sendMessage?chat_id=#{@api_id}&text=#{text}")
+    
+     HTTParty.post("https://api.telegram.org/bot#{@api_key}/sendMessage",
+   
+       Headers: {
+         'Content-Type' => 'application/json'
+       },
+       body: {
+        api_id: @api_id,
+       text: text
+       }.to_json
+     )
+
     redirect_to root_path, notice: "Successful"
   end
-  
+
 end
---
+++ config/initializers/telegram.rb
@@ -0,0 +1,2 @@
+@api_key = "5609590504:AAHWdpmAzwhmqcUPp0MbSHgOq6OfLvWX0lw"
+@api_id = "5333385500"
